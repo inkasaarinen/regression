21d091acf0d91dc601e4b28b00b86760be07fbab
muti <inka.j.saarinen@gmail.com>
2017-12-10 12:38:59 +0200

    xx

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ def predict_house_price():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=port)
 
 
