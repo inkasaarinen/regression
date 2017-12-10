ea324545227ae0f4aab45f7cd8fabcf5e16d0bdf
muti <inka.j.saarinen@gmail.com>
2017-12-10 09:14:00 +0200

    added files

---
+++ fit_model.py
@@ -0,0 +1,27 @@
+from sklearn import linear_model
+import numpy
+
+#filename = "/home/ekhnaton/Documents/Inka/housing.csv"
+#filename = "/home/muti/Downloads/housing.csv"
+filename = 'housing.csv'
+data=numpy.genfromtxt(filename,delimiter=',',dtype=float,skip_header=1)
+features = data[:,0:5]
+targets = data[:,5]
+
+# extract variables to correct format
+
+model = linear_model.LinearRegression()
+model.fit(features, targets) # array of input variables, array of output variables
+model.coef_ # contains the coefficients
+model.intercept_
+model_coef_with_intercept = numpy.append(model.coef_,model.intercept_)
+
+# save model
+#output_filename = "/home/ekhnaton/Documents/Inka/housing_model.txt"
+#output_filename = "/home/muti/Documents/housing_model.txt"
+output_filename = 'housing_model.txt'
+with open(output_filename, 'ab') as outfile:
+    numpy.savetxt(outfile, model_coef_with_intercept)
+
+# check results / do cross-validation tjsp.
+
---
+++ predict.py
@@ -0,0 +1,42 @@
+from flask import Flask
+import numpy
+from flask import json
+from flask import abort
+from flask import request
+
+app = Flask(__name__)
+@app.route('/predict', methods=['GET', 'POST'])
+def predict_house_price():
+  # check and parse input json
+  try:
+    input1 = request.json
+    crime_rate = float(input1['crime_rate'])
+    avg_number_of_rooms = float(input1['avg_number_of_rooms'])
+    distance_to_employment_centers = float(input1['distance_to_employment_centers'])
+    property_tax_rate = float(input1['property_tax_rate'])
+    pupil_teacher_ratio = float(input1['pupil_teacher_ratio'])
+    crime_rate = 1.0   
+  except:
+    abort(400)
+  
+  intercept = 1.0
+  data = [crime_rate, avg_number_of_rooms, distance_to_employment_centers, property_tax_rate, pupil_teacher_ratio, intercept]
+  
+  # read model
+  #filename = "/home/ekhnaton/Documents/Inka/housing_model.txt"
+  #filename = "/home/muti/Documents/housing_model.txt"
+  filename = 'housing_model.txt'
+  model=numpy.genfromtxt(filename,delimiter=',',dtype=float,skip_header=0)
+  
+  # compute prediction
+  #prediction = data*model
+  
+  # format as json
+  prediction = {}
+  prediction['house_value'] = sum(data*model)
+  #prediction['stddev'] = ??
+  prediction_json = json.dumps(prediction)
+  
+  
+  return prediction_json
+
